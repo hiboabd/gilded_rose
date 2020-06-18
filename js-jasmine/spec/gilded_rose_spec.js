cc8a74efd65f568abeec1854e1da2735b613465c
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-18 13:30:15 +0100

    Refactored test to use beforeEach

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,10 +1,14 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
+  var gildedRose
+
+  beforeEach(() => {
+    gildedRose = new Shop([ new Item("Test item", 0, 0) ]);
+  });
 
   it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("foo");
+    expect(items[0].name).toEqual("Test item");
   });
 
 });
