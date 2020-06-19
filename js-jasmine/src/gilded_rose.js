df3f2f8ba3e691bad1ffc18b71d8a7347bd5f88a
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-19 15:19:15 +0100

    Refactored code to check name of item against array

--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -46,52 +46,40 @@ class Shop {
       // if the items quality is between the limits
 
       if(this.isqualityLimitExceeded(this.items[i]) === false){
-        // if the item is not Aged brie and Backstage pass (should be or)
-        if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          // if the items name is Sulfuras, hand of ragnaros
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            // decrease the quality of the item by itself minus 1
-            this.updateOtherItemsQuality(this.items[i])
-          }
+        // if the item is not Aged brie or Backstage pass or sulfuras
+        if (this.specialItems.includes(this.items[i].name) === false) {
+          this.updateOtherItemsQuality(this.items[i])
         } else {
-          // increase the quality by itself + 1
-          this.items[i].quality = this.items[i].quality + 1;
-          // if the items name is backstage pass
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            // if the number of days left to sell the item is less than 11
-            if (this.items[i].sellIn < 11) {
-              // increase the quality of the item by itself + 1
-              this.items[i].quality = this.items[i].quality + 1;
-            }
-            // if the number of days left to sell the item is less than 6
-            if (this.items[i].sellIn < 6) {
-              // increase the quality of the item by itself + 1
-              this.items[i].quality = this.items[i].quality + 1;
+          if(this.items[i].name != 'Sulfuras, Hand of Ragnaros'){
+            // increase the quality by itself + 1
+            this.updateAgedBrieQuality(this.items[i]);
+            // if the items name is backstage pass
+            if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+              // if the number of days left to sell the item is less than 11
+              if (this.items[i].sellIn < 11) {
+                // increase the quality of the item by itself + 1
+                this.items[i].quality = this.items[i].quality + 1;
+              }
+              // if the number of days left to sell the item is less than 6
+              if (this.items[i].sellIn < 6) {
+                // increase the quality of the item by itself + 1
+                this.items[i].quality = this.items[i].quality + 1;
+              }
             }
           }
         }
         // if the number of days left to sell the item is less than 0
         if (this.items[i].sellIn < 0) {
-          // and the items name is not aged brie
-          if (this.items[i].name != 'Aged Brie') {
-            // and the items name is not backstage pass
-            if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-              // and the items name is not equal to sulfuras, hand of ragnaros
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                // decrease the items quality by itself - 1
-                this.items[i].quality = this.items[i].quality - 1;
-              }
+          // and the items name is not aged brie, backstage pass or sulfuras
+          if (this.specialItems.includes(this.items[i].name) === false) {
+              this.items[i].quality = this.items[i].quality - 1;
             } else {
               // decrease the items quality by itself
               this.items[i].quality = this.items[i].quality - this.items[i].quality;
             }
-          } else {
-            this.updateAgedBrieQuality(this.items[i])
           }
         }
       }
-    }
-
     // return the array of items
     return this.items;
   }
