f77c2f98e9859521db109d5493c169bb41b116ae
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-19 19:36:12 +0100

    Refactoring methods - changed for to foreach loop for readibility

--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -35,51 +35,44 @@ class Shop {
   }
 
   updateBackstagePassQuality(item){
-    if(item.sellIn < 10 && item.sellIn > 5){
-      item.quality = item.quality + 2;
-    } else if(item.sellIn < 5 && item.sellIn > 0) {
-      item.quality = item.quality + 3;
-    } else if (item.sellIn < 0) {
+    if(item.sellIn < 0){
       item.quality = item.quality - item.quality;
+    } else if(item.sellIn < 5){
+      item.quality = item.quality + 3;
+    } else if(item.sellIn < 10){
+      item.quality = item.quality + 2;
     } else {
       item.quality = item.quality + 1;
     }
   }
 
   updateQuality() {
-    // looping over the list of shop items starting from beginning to end
-    for (var i = 0; i < this.items.length; i++) {
-      // if the items name is not sulfuras hand of ragnaros
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        // decrease the number of days left to sell the item by 1
-        this.decreaseSellIn(this.items[i])
+    this.items.forEach((item, i) => {
+      if (item.name !== this.specialItems[0]) {
+        this.decreaseSellIn(item)
       }
 
-      // if the items quality is between the limits
-      if(this.isqualityLimitExceeded(this.items[i]) === false){
-        // if the item is not Aged brie or Backstage pass or sulfuras
-        if (this.specialItems.includes(this.items[i].name) === false) {
-          this.updateOtherItemsQuality(this.items[i])
-          // if the number of days left to sell the item is less than 0
-          if (this.items[i].sellIn < 0) {
-            this.updateOtherItemsQuality(this.items[i])
+      if(this.isqualityLimitExceeded(item) === false){
+        if (this.specialItems.includes(item.name) === false) {
+          this.updateOtherItemsQuality(item)
+          if (item.sellIn < 0) {
+            this.updateOtherItemsQuality(item)
           }
         }
 
-        if(this.items[i].name === this.specialItems[2]){
-            this.updateAgedBrieQuality(this.items[i]);
+        if(item.name === this.specialItems[2]){
+            this.updateAgedBrieQuality(item);
         }
 
-        if(this.items[i].name === this.specialItems[1]){
-          this.updateBackstagePassQuality(this.items[i]);
+        if(item.name === this.specialItems[1]){
+          this.updateBackstagePassQuality(item);
         }
-
       }
-    }
-    // return the array of items
+    });
     return this.items;
   }
 }
+
 module.exports = {
   Item,
   Shop
