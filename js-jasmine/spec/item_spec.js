cd1b1560090276a7fc0387aee5c0407b76d79ef7
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-19 12:04:58 +0100

    Commented out new classes and respective spec files

--- js-jasmine/spec/item_spec.js
+++ js-jasmine/spec/item_spec.js
@@ -1,21 +1,21 @@
-var {Item} = require('../src/item.js');
-
-describe("Item", function() {
-  var testItem
-
-  beforeEach(() => {
-    testItem = new Item("Test item", 30, 10);
-  });
-
-  it("Test item has a name", function() {
-    expect(testItem.name).toEqual("Test item")
-  });
-
-  it("Test item has a sellIn property", function() {
-    expect(testItem.sellIn).toEqual(30)
-  });
-
-  it("Test item has a quality property", function() {
-    expect(testItem.quality).toEqual(10)
-  });
-});
+// var {Item} = require('../src/item.js');
+//
+// describe("Item", function() {
+//   var testItem
+//
+//   beforeEach(() => {
+//     testItem = new Item("Test item", 30, 10);
+//   });
+//
+//   it("Test item has a name", function() {
+//     expect(testItem.name).toEqual("Test item")
+//   });
+//
+//   it("Test item has a sellIn property", function() {
+//     expect(testItem.sellIn).toEqual(30)
+//   });
+//
+//   it("Test item has a quality property", function() {
+//     expect(testItem.quality).toEqual(10)
+//   });
+// });
--- js-jasmine/src/item.js
+++ js-jasmine/src/item.js
@@ -1,11 +1,11 @@
-class Item {
-  constructor(name, sellIn, quality){
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
-}
-
-module.exports = {
-  Item
-}
+// class Item {
+//   constructor(name, sellIn, quality){
+//     this.name = name;
+//     this.sellIn = sellIn;
+//     this.quality = quality;
+//   }
+// }
+//
+// module.exports = {
+//   Item
+// }
